f491e46a73147143ba5991cf5f067c0cf6404c13
JungWon Park <firstman9@protonmail.com>
2023-10-01 08:01:59 +0900

    Merged next05 repository into nextjs-tutorials

---
+++ next05/lib/getWikiResults.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default async function getWikiResults(searchTerm: string) {
+  
+	const searchParams = new URLSearchParams({
+	action: 'query',
+	generator: 'search',
+	gsrsearch: searchTerm,
+	gslimit: '20',
+	prop: 'pageimages|extracts',
+	exchars: '100',
+	exintro: 'true',
+	explaintext: 'true',
+	exlmit: 'max',
+	format: 'json',
+	origin: '*'
+  })
+
+  const response = await fetch(`https://en.wikipedia.org/w/api.php?${searchParams.toString()}`)
+  
+  return response.json()
+}
